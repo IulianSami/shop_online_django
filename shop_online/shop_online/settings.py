e5ab3f7f8595b0f3d7a693cac7c361a7f16d9ab8
IulianSami <iuliansami@gmail.com>
2025-05-14 21:07:44 +0300

    Updated CSS Style

--- shop_online/shop_online/settings.py
+++ shop_online/shop_online/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig', # manual add app store
+    'django_extensions',  
 ]
 
 MIDDLEWARE = [
